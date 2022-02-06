2555f8c66ed332b3cfbadc64f41be1b224f955cf
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2022-02-06 06:14:19 +0300

    Güncellendi!

---
+++ rchesapla.js
@@ -0,0 +1,134 @@
+//Points Per Win Functions
+        function CoinSelect()
+      { 
+       document.getElementById("CoinPoints").value=
+       document.getElementById("Coinclick-lvl").value;
+      }
+
+      function BlasterSelect()
+      { 
+       document.getElementById("BlasterPoints").value=
+       document.getElementById("Blaster-lvl").value;
+      }
+      
+      function FlappySelect()
+      { 
+       document.getElementById("FlappyPoints").value=
+       document.getElementById("Flappy-lvl").value;
+      }
+      
+      function CryptonoidSelect()
+      { 
+       document.getElementById("CryptonoidPoints").value=
+       document.getElementById("Cryptonoid-lvl").value;
+      }
+      
+      function MatchSelect()
+      { 
+       document.getElementById("MatchPoints").value=
+       document.getElementById("Match-lvl").value;
+      }
+      
+      function CryptoSelect()
+      { 
+       document.getElementById("CryptoPoints").value=
+       document.getElementById("Crypto-lvl").value;
+      }
+      
+      function TwentyFourtyEightSelect()
+      { 
+       document.getElementById("TwentyFourtyEightPoints").value=
+       document.getElementById("TwentyFourtyEight-lvl").value;
+      }
+      
+      function FlipSelect()
+      { 
+       document.getElementById("FlipPoints").value=
+       document.getElementById("Flip-lvl").value;
+      }
+      
+      function DrSelect()
+      { 
+       document.getElementById("DrPoints").value=
+       document.getElementById("Dr-lvl").value;
+      }
+      
+      function SurferSelect()
+      { 
+       document.getElementById("SurferPoints").value=
+       document.getElementById("Surfer-lvl").value;
+      }
+	  
+	  function LamboSelect()
+      { 
+       document.getElementById("LamboRiderPoints").value=
+       document.getElementById("LamboRider-lvl").value;
+      }
+
+      //Lvl Conquered Functions
+      
+      function TripleCoin()
+      { 
+       document.getElementById("TripleCoin").value = 3*document.getElementById("Coinclick-lvl").value
+        if (isNaN(document.getElementById("TripleCoin").value)) document.getElementById("TripleCoin").value = 0; 
+      }
+      
+      function TripleBlaster()
+      { 
+       document.getElementById("TripleBlaster").value = 3*document.getElementById("Blaster-lvl").value
+        if (isNaN(document.getElementById("TripleBlaster").value)) document.getElementById("TripleBlaster").value = 0; 
+      }
+      
+      function TripleFlappy()
+      { 
+       document.getElementById("TripleFlappy").value = 3*document.getElementById("Flappy-lvl").value
+        if (isNaN(document.getElementById("TripleFlappy").value)) document.getElementById("TripleFlappy").value = 0; 
+      }
+      
+      function TripleCryptonoid()
+      { 
+       document.getElementById("TripleCryptonoid").value = 3*document.getElementById("Cryptonoid-lvl").value
+        if (isNaN(document.getElementById("TripleCryptonoid").value)) document.getElementById("TripleCryptonoid").value = 0; 
+      }
+      
+      function TripleMatch()
+      { 
+       document.getElementById("TripleMatch").value = 3*document.getElementById("Match-lvl").value
+        if (isNaN(document.getElementById("TripleMatch").value)) document.getElementById("TripleMatch").value = 0; 
+      }
+      
+      function TripleCrypto()
+      { 
+       document.getElementById("TripleCrypto").value = 3*document.getElementById("Crypto-lvl").value
+        if (isNaN(document.getElementById("TripleCrypto").value)) document.getElementById("TripleCrypto").value = 0; 
+      }
+      
+      function TripleTwentyFourtyEight()
+      { 
+       document.getElementById("TripleTwentyFourtyEight").value = 3*document.getElementById("TwentyFourtyEight-lvl").value
+        if (isNaN(document.getElementById("TripleTwentyFourtyEight").value)) document.getElementById("TripleTwentyFourtyEight").value = 0; 
+      }
+      
+      function TripleFlip()
+      { 
+       document.getElementById("TripleFlip").value = 3*document.getElementById("Flip-lvl").value
+        if (isNaN(document.getElementById("TripleFlip").value)) document.getElementById("TripleFlip").value = 0; 
+      }
+      
+      function TripleDr()
+      { 
+       document.getElementById("TripleDr").value = 3*document.getElementById("Dr-lvl").value
+        if (isNaN(document.getElementById("TripleDr").value)) document.getElementById("TripleDr").value = 0; 
+      }
+      
+      function TripleSurfer()
+      { 
+       document.getElementById("TripleSurfer").value = 3*document.getElementById("Surfer-lvl").value
+        if (isNaN(document.getElementById("TripleSurfer").value)) document.getElementById("TripleSurfer").value = 0; 
+      }
+	  
+	  function TripleLambo()
+      { 
+       document.getElementById("TripleLambo").value = 3*document.getElementById("LamboRider-lvl").value
+        if (isNaN(document.getElementById("TripleLambo").value)) document.getElementById("TripleLambo").value = 0; 
+      }
